1c4a521d2893e103adf7f46daadaeeaea4e37784
Jovann Contreras <jcontreras@codestack.co>
2022-10-20 15:00:25 -0700

    comment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿
+﻿// Jovann Contreras
+// Date 10/19/2022
+
+// Getting the inputs of ints and strings and reversing them.
+// Started to validate the string and int and reverse them
+
 Console.Clear();
 bool validNum;
 long resultNum = 0;
